7d48e032ee8151dd37516704732e8084ef80b8e4
Sergei Rakov <sergei@snoonu.com>
2022-04-15 10:42:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/coroutinesexample/BasicCoroutinesFragment.kt
@@ -0,0 +1,14 @@
+package com.example.coroutinesexample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class BasicCoroutinesFragment : Fragment(R.layout.fragment_basic_coroutines) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
